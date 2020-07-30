b5e4914d55332c98d4c1f1ad28ea2c14e0ba8fc8
gpolavaram0 <goutham.polavaram@gmail.com>
2020-07-30 15:48:47 -0400

    test1

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def welcome():
 @app.route("/about.html")
 def team():
 
-    return render_template("index.html")
+    return render_template("docs/index.html")
 
     
 if __name__ == '__main__':
