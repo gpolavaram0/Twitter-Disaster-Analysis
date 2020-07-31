f2481763c97917487559b046105a8b50a076eefc
gpolavaram0 <60547556+gpolavaram0@users.noreply.github.com>
2020-07-31 18:24:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,14 +18,28 @@ app._static_folder = 'static/'
 # # Connects to the database using the app config
 
 #################################################
+
+new_model = tf.keras.models.load_model('76_64.h5')
+
+padded = [[   2,    1,    1,    7, 2910,   38, 2792,    1, 2629,    1,   12,           2, 2982,    1, 1052,   42,    1,  113,    5,    1,    2, 2105,         818,   28, 2629,    1,    1, 3228]]
+
+# print(new_model.predict(padded))
+#################################################
+
+
+
+#################################################
 # Flask Routes
 #################################################
 
 @app.route("/")
 def welcome():
-
+    
+    test4 = str(new_model.predict(padded))
+    
+    test3 = test_func()
     # return render_template("index.html")
-    return "sadgfd"
+    return test4
 
 @app.route("/index.html")
 def index():
